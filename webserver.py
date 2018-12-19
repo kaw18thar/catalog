98c950130cfc528734876519c996b5094c018b5f
kaw18thar <33849704+kaw18thar@users.noreply.github.com>
2018-12-20 02:38:27 +0300

    edit:do_POST copy the lesson's code for it

--- webserver.py
+++ webserver.py
@@ -40,7 +40,7 @@ class webserverHandler(BaseHTTPRequestHandler):
     
     def do_POST(self):
         try:
-            self.send_reponse(301)
+            self.send_response(301)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             ctype, pdict = cgi.parse_header(
@@ -50,14 +50,14 @@ class webserverHandler(BaseHTTPRequestHandler):
                 messagecontent = fields.get('message')
             output = ""
             output += "<html><body>"
-            output += "<h2> Okay, How About This: </h2>"
-            output += "<h2> %s </h2>" % messagecontent[0]
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
             output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
             self.wfile.write(output)
             print output
         except:
-            pass
+
 def main():
     try:
         port = 8080
